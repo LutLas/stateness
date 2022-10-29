4061064c572ee1944c3772e0644de347f705cf8a
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-10-29 20:16:53 +0200

    Initial Commit

---
+++ stateness/statenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  statenessApp.swift
+//  stateness
+//
+//  Created by Lutaka Muyoba Chihota on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct statenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
